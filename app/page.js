c676108047c836208e88d31c7d37247015a01999
sarayu19724 <madigundusarayu@gmail.com>
2024-12-26 15:28:28 +0530

    changed app bar

--- app/page.js
+++ app/page.js
@@ -2,9 +2,17 @@
 import styles from "./page.module.css";
 import {useState} from "react";
 import React from "react";
-import Button from '@mui/material/Button';
+
 import MyCard from './card';
 import DialogForm from "./form";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
 
 export default function Home() {
   const [Login,setLogin] = useState(false);
@@ -26,79 +34,50 @@ export default function Home() {
     Description: '',
     Imageurl: '',
   }); 
-  const handleChange = (e) => { const { name, value } = e.target; setValues({ ...values, [name]: value, }); };
+  
   
     const cards = [ 
       { 
-        name: 'Machine Learning', Description: 'An advanced course on machine learning algorithms',Code:'Code:CS711', Credits:'Credits:9',image: 'https://www.hfsresearch.com/wp-content/uploads/2019/09/5b5f1a8e048a3267059501.png' }, 
-      {name: 'Data Structures and algorithms', Description: 'Learn about arrays,linked lists and more',Code:'Code:ESO207',Credits:'Credits:11', image: 'https://www.seedprogramming.org/wp-content/uploads/2023/02/PicsArt_03-24-07.25.58-1.webp' }, 
-      { name: 'Fluid Mechanics', Description: 'Fundamentals of fluid mechanics and applications',Code:'Code:ME302',Credits:'Credits:9', image: 'https://edurev.gumlet.io/AllImages/original/ApplicationImages/CourseImages/dfb3dc65-ad56-4554-a184-b151ef2d70a4_CI.jpg' }, 
-      {name: 'Introduction to electronics', Description: 'An introductory course of electric circuits & applications',Code:'Code:ESC201', credits:'Credits:14',image: 'https://th.bing.com/th/id/OIP.d8WM4VWYPHKx09WEcAD3eAHaE7?rs=1&pid=ImgDetMain'}];
+        Name: 'Machine Learning', Description: 'An advanced course on machine learning algorithms',Code:'Code:CS711', Credits:'Credits:9',image: 'https://www.hfsresearch.com/wp-content/uploads/2019/09/5b5f1a8e048a3267059501.png' }, 
+      {Name: 'Data Structures and algorithms', Description: 'Learn about arrays,linked lists and more',Code:'Code:ESO207',Credits:'Credits:11', image: 'https://www.seedprogramming.org/wp-content/uploads/2023/02/PicsArt_03-24-07.25.58-1.webp' }, 
+      { Name: 'Fluid Mechanics', Description: 'Fundamentals of fluid mechanics and applications',Code:'Code:ME302',Credits:'Credits:9', image: 'https://edurev.gumlet.io/AllImages/original/ApplicationImages/CourseImages/dfb3dc65-ad56-4554-a184-b151ef2d70a4_CI.jpg' }, 
+      {Name: 'Introduction to electronics', Description: 'An introductory course of electric circuits & applications',Code:'Code:ESC201', credits:'Credits:14',image: 'https://th.bing.com/th/id/OIP.d8WM4VWYPHKx09WEcAD3eAHaE7?rs=1&pid=ImgDetMain'}];
 
 
 
     return (
     <>
-    <div className={styles.header}>
-        <ul>
-          <li className={styles.h1}>Course Helper</li>
-          <li className={styles.h3}><button className={styles.h2} onClick={()=> setLogin(true)}>LOGIN</button></li>
-        </ul>
-    
-     {
-      Login&&
-      <div className={styles.popup}>
+       <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
           
-        <div className={styles.pop}>
-         <div className={styles.pop1}>
-          
-         </div>
-          <div className={styles.Pop}>
-            
-            <button className={styles.button} onClick={()=> setLogin(false)}>x</button>
-            <h4 className={styles.h4}>Login</h4>
-            <form className={styles.form} action='#'>
-              <div className={styles.input_box}>
-                <span className={styles.icon}>
-                </span>
-                <input className={styles.input} type="email" required />
-                <label>Email</label>
-              </div>
-              <div className={styles.input_box}>
-              <span className={styles.icon}>
-              </span>
-                <input className={styles.input} type="password" required />
-                <label>Password</label>
-              </div>
-              <div className={styles.remember}>
-                <label><input type="checkbox"></input>
-                Remember me </label>
-                <a href="#" >Forgot Password?</a>
-              </div>
-              
-              
-            </form>
-            <button type="submit" className={styles.btn}>Login</button>
-          </div>
-        </div>
-      </div>
-    
-     } 
-    </div>  
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Course Helper
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
       <div >
       <div style={{ display: 'flex', gap: '16px', flexWrap: 'wrap' }}>
-         {cards.map((card, index) => ( 
+      {cards.map((card, index) => (
           <MyCard
-           key={index} name={card.name} Description={card.Description} image={card.image} Code={card.Code} Credits={card.Credits}  /> 
+            key={index}
+            index={index + 1}
+            Name={card.Name}
+            Description={card.Description}
+            Code={card.Code}
+            Credits={card.Credits}
+            image={card.image}
+          />
+        ))}  
            
-           ))} 
-           
-
+          
 
       </div>
         
         <React.Fragment>
-      <Button variant="outlined" onClick={handleClickOpen}>
+      <Button style={{marginLeft: 1300,marginTop: -30}} variant="outlined" onClick={handleClickOpen}>
         Add Course
       </Button>
       <DialogForm open={open} handleClose={handleClose} values={values}/>
